d4148465de8402cea9726d8dcac9a859e3358a63
Andrew Drogalis <108765079+drogalis@users.noreply.github.com>
2024-10-13 16:43:14 -0400

    Fixed Implementation of Find Benchmark

--- benchmarks/flat-rb-tree-benchmark.cpp
+++ benchmarks/flat-rb-tree-benchmark.cpp
@@ -7,11 +7,11 @@
 
 #include "dro/flat-rb-tree.hpp"
 #include <chrono>
-#include <cstdlib>
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
-#include <random>
 #include <map>
+#include <random>
 #include <vector>
 
 #if __has_include(<boost/container/flat_map.hpp> )
@@ -26,16 +26,17 @@ struct alignas(4) Test {
 };
 
 int main() {
-  int iterations = 10'000;
+  int iterations = 100'000;
 
   // Generate Vector of Random Ints
   std::vector<int> randInts(iterations);
+  std::vector<std::pair<Test, Test>> findKeys(iterations);
   for (auto& i : randInts) { i = rand(); }
 
   std::cout << "Dro FlatMap: \n";
 
   // Insertion Benchmark
-  dro::FlatMap<Test, Test, uint8_t> dro_;
+  dro::FlatMap<Test, Test, uint32_t> dro_;
   auto start = std::chrono::high_resolution_clock::now();
   for (auto i : randInts) { dro_.emplace(Test(i), Test(i)); }
   auto stop = std::chrono::high_resolution_clock::now();
@@ -48,14 +49,19 @@ int main() {
             << " ns.\n";
 
   // Find Benchmark
+  int idx {};
   start = std::chrono::high_resolution_clock::now();
-  for (auto i : randInts) { dro_.find(Test(i)); }
+  for (auto i : randInts) {
+    findKeys[idx] = *(dro_.find(Test(i)));
+    ++idx;
+  }
   stop = std::chrono::high_resolution_clock::now();
 
   std::cout << "Total find time: "
             << std::chrono::duration_cast<std::chrono::nanoseconds>(stop -
                                                                     start)
-                   .count()
+                       .count() /
+                   iterations
             << " ns.\n";
 
   // Remove Benchmark
@@ -89,14 +95,19 @@ int main() {
             << " ns.\n";
 
   // Find Benchmark
+  idx   = 0;
   start = std::chrono::high_resolution_clock::now();
-  for (auto i : randInts) { stl_.count(Test(i)); }
+  for (auto i : randInts) {
+    findKeys[idx] = *(stl_.find(Test(i)));
+    ++idx;
+  }
   stop = std::chrono::high_resolution_clock::now();
 
   std::cout << "Total find time: "
             << std::chrono::duration_cast<std::chrono::nanoseconds>(stop -
                                                                     start)
-                   .count()
+                       .count() /
+                   iterations
             << " ns.\n";
 
   // Remove Benchmark
@@ -129,14 +140,19 @@ int main() {
             << " ns.\n";
 
   // Find Benchmark
+  idx   = 0;
   start = std::chrono::high_resolution_clock::now();
-  for (auto i : randInts) { boost_.find(Test(i)); }
+  for (auto i : randInts) {
+    findKeys[idx] = *(boost_.find(Test(i)));
+    ++idx;
+  }
   stop = std::chrono::high_resolution_clock::now();
 
   std::cout << "Total find time: "
             << std::chrono::duration_cast<std::chrono::nanoseconds>(stop -
                                                                     start)
-                   .count()
+                       .count() /
+                   iterations
             << " ns.\n";
 
   // Remove Benchmark
